27b2480b615e676ebf16e2599b252573793c74ac
amod.mahajan <amod.mahajan@hotmail.com>
2023-09-18 17:25:38 -0400

    Revert "Added hello world 100"
    This reverts commit 12fadc18e1d1807b69401f9f28166a684c23d1d8.

--- src/Utils/HelloWorld.java
+++ src/Utils/HelloWorld.java
@@ -3,7 +3,7 @@ package Utils;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world 100");
+        System.out.println("Hello world");
         System.out.println("Hello world 1");
         System.out.println("Hello world 2");
     }
